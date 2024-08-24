7da584be9e3e1ebde93f69ed08a05e4cc46d552e
unknown <harshpatel0818.com>
2024-08-23 22:04:02 -0500

    reset

--- keys.py
+++
@@ -1,4 +0,0 @@
-alpaca_api_key = "PKTFVMUC5JA33B5U42PC"
-alpaca_api_secret_key = "RLb0SSNMLsWK3v3NkYAB1TCt5YVKKeoUMDgnYDJy"
-alpaca_endpoint = "https://paper-api.alpaca.markets/v2"
-twoFA_code = "b90b3d67-fcc4-4ddd-a8b9-660780cb07db"
